4c294edb6c3ee38c58bb834aaa269fd483c79971
Maxim Sarukhanov <msarukhanov@descartes.com>
2022-06-17 08:58:16 +0300

    clean up

---
+++ Utils/ObjectTypeHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Xamarin.Android.Mvvm.App.Utils
+{
+    public static class ObjectTypeHelper
+    {
+        public static T Cast<T>(this Java.Lang.Object obj) where T : class
+        {
+            if ((obj == null)) return null;
+            var propertyInfo = obj.GetType().GetProperty("Instance");
+            return propertyInfo == null ? null : propertyInfo.GetValue(obj, null) as T;
+        }
+
+        public static Java.Lang.Object ReverseCast<T>(T obj) where T : class
+        {
+            var propertyInfo = obj.GetType().GetProperty("Instance");
+            return propertyInfo == null ? null : propertyInfo.GetValue(obj, null) as Java.Lang.Object;
+        }
+    }
+}
